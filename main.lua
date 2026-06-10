d6a80e938c2f2fabac392c37948e83e8aaac07a3
CraySone <jd.office@mailbox.org>
2026-06-11 02:01:54 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ local power_ranger_on = {
     name = "Power Ranger ON",
     author = "CraySone",
     desc = "PvP Overlay and self tracking",
-    version = "1.4.7"
+    version = "1.5.0"
 }
 
 local active = false
